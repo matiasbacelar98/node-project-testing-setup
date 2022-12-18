c5248315b90b0e841ed5ddc82018a91212db301a
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-18 02:45:18 -0300

    Add data to database before testing it

--- src/modules/user/user.test.ts
+++ src/modules/user/user.test.ts
@@ -1,5 +1,7 @@
 import supertest from 'supertest';
 import setupTestDB from '../../utils/setupTestDB';
+
+import UserModel from '../user/user-model';
 import { createServer } from '../../utils/server';
 
 const app = createServer();
@@ -9,6 +11,21 @@ const user = {
   email: 'test@gmail.com',
   username: 'test',
 };
+
+const users = [
+  {
+    email: 'test1@gmail.com',
+    username: 'test1',
+  },
+  {
+    email: 'test2@gmail.com',
+    username: 'test2',
+  },
+  {
+    email: 'test3@gmail.com',
+    username: 'test3',
+  },
+];
 
 // Setup DB
 setupTestDB('user-test-db');
@@ -23,5 +40,13 @@ describe('user route', () => {
   test('Should save user to database', async () => {
     const response = await supertest(app).post('/api/user').send(user);
     expect(response.statusCode).toBe(200);
+  });
+
+  test('Should return all the users in the database', async () => {
+    await UserModel.insertMany(users);
+    const response = await supertest(app).get('/api/user').send();
+
+    const usersAmount = response.body.users.length;
+    expect(usersAmount).toBe(3);
   });
 });
